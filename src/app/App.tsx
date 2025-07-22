d7ba36260d62faf3bb404d7f4baab7bb7a0c9b35
Quino <michurinec007@gmail.com>
2025-07-22 17:13:14 +0300

    изменил глобальные стили

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,15 +2,18 @@ import { HomePage, NotFound404 } from '@/pages';
 import '@appStyles/fonts.css';
 import '@appStyles/normalize.css';
 import '@appStyles/global.css';
+import '@appStyles/theme.css';
 
 import { Route, Routes } from 'react-router-dom';
 import ProfilePage from '@/pages/Profile/ProfilePage';
 import ProfileLayout from '@/pages/Profile/ProfileLayout';
 import PlaceholderPage from '@/pages/Profile/PlaceholderPage';
 
+import { ThemeProvider } from '@app/styles/ThemeProvider';
+
 const App = () => {
 	return (
-		<>
+		<ThemeProvider>
 			<Routes /*location={backgroundLocation || location}*/>
 				<Route path='/' element={<HomePage />} />
 				<Route path='*' element={<NotFound404 />} />
@@ -54,7 +57,7 @@ const App = () => {
 					/>
 				</Route>
 			</Routes>
-		</>
+		</ThemeProvider>
 	);
 };
 
