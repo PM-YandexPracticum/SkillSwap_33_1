d99adda09eb746e6e10327b7b28fd4ee0dbe0c64
Quino <michurinec007@gmail.com>
2025-07-25 10:13:00 +0300

    изображение для темной темы

--- src/pages/NotFound404/NotFound404.tsx
+++ src/pages/NotFound404/NotFound404.tsx
@@ -1,8 +1,18 @@
+import { useTheme } from '../../app/styles/ThemeProvider';
+import React from 'react';
+
 export const NotFound404 = () => {
+	const { theme } = useTheme();
+
+	const error404Image =
+		theme === 'dark'
+			? 'assets/images/error-404-dark.svg'
+			: 'assets/images/error-404-light.svg';
+
 	return (
 		<>
 			<h1>404</h1>
-			<img src='assets/images/error-404.svg' alt='' />
+			<img src={error404Image} alt='Page not found' />
 		</>
 	);
 };
