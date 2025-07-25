b7132b083bccb3577bcba5607fb701e6b3b39254
Quino <michurinec007@gmail.com>
2025-07-25 10:12:03 +0300

    темная тема в HomePage

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -2,15 +2,28 @@ import styles from './HomePage.module.css';
 import ClockIcon from '@icons/clock.svg?react';
 import EditIcon from '@icons/edit.svg?react';
 import BookIcon from '@icons/book.svg?react';
+import { useTheme } from '../../app/styles/ThemeProvider';
 
 export const HomePage = () => {
+	const { theme } = useTheme();
+
+	const error404Image =
+		theme === 'dark'
+			? 'assets/images/error-404-dark.svg'
+			: 'assets/images/error-404-light.svg';
+
+	const error500Image =
+		theme === 'dark'
+			? 'assets/images/error-500-dark.svg'
+			: 'assets/images/error-500-light.svg';
+
 	return (
 		<>
 			<h1 className={styles.heading}>Home Page</h1>
 			<div>
 				<img src='assets/icons/testIcon.svg' alt='' />
-				<img src='assets/images/error-404.svg' alt='' />
-				<img src='assets/images/error-500.svg' alt='' />
+				<img src={error404Image} alt='Error 404' />
+				<img src={error500Image} alt='Error 500' />
 				<img src='assets/images/light-bulb.svg' alt='' />
 			</div>
 			<div>
