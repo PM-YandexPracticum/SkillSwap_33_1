54e190d72cf1da7a0c06ef44b9f9cb1cbb9f91c7
Kirill Makarov <kirillmakarov131995@yandex.ru>
2025-07-29 22:43:17 +0300

    feature/userCards_and_tag

---
+++ src/pages/TestPage/TestPage.tsx
@@ -0,0 +1,245 @@
+// import styles from './TestPage.module.css';
+import { CardUser } from '@/widgets/UserCard/CardUser';
+import type { User } from '@/types';
+
+const tempUsers: User[] = [
+	{
+		id: 'f3f43f3f34f',
+		name: 'Максим',
+		location: 'Москва',
+		gender: 'male',
+		age: '23',
+		description:
+			'Привет! Люблю ритм, кофе по утрам и людей, которые не боятся пробовать новое',
+		avatarUrl: '/assets/images/profile-pictures/Image3.svg',
+		skillCanTeach: [
+			{
+				skill: 'Бизнес-план',
+				categoryId: 0,
+				subcategory: 0,
+			},
+			{
+				skill: 'Английский язык',
+				categoryId: 1,
+				subcategory: 0,
+			},
+		],
+		subcategoriesWantToLearn: [
+			{
+				skill: 'Бизнес-план',
+				categoryId: 0,
+				subcategory: 0,
+			},
+			{
+				skill: 'Английский язык',
+				categoryId: 1,
+				subcategory: 0,
+			},
+			{
+				skill: 'Тайм менеджмент',
+				categoryId: 2,
+				subcategory: 0,
+			},
+			{
+				skill: 'Медитация',
+				categoryId: 3,
+				subcategory: 0,
+			},
+		],
+	},
+	{
+		id: 'fsdf3f34f3f43',
+		name: 'Виктория',
+		location: 'Таджикистан',
+		gender: 'female',
+		age: '89',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, delectus dolore cupiditate tenetur quasi suscipit reprehenderit rem aliquam vero perspiciatis ducimus doloremque fugit id eveniet nobis adipisci consequatur. Autem, qui?',
+		avatarUrl: '/assets/images/profile-pictures/Image2.svg',
+		skillCanTeach: [
+			{
+				skill: 'Бизнес-план',
+				categoryId: 0,
+				subcategory: 0,
+			},
+			{
+				skill: 'Английский язык',
+				categoryId: 1,
+				subcategory: 0,
+			},
+		],
+		subcategoriesWantToLearn: [
+			{
+				skill: 'Бизнес-план',
+				categoryId: 0,
+				subcategory: 0,
+			},
+			{
+				skill: 'Английский язык',
+				categoryId: 1,
+				subcategory: 0,
+			},
+			{
+				skill: 'Тайм менеджмент',
+				categoryId: 2,
+				subcategory: 0,
+			},
+			{
+				skill: 'Медитация',
+				categoryId: 3,
+				subcategory: 0,
+			},
+		],
+	},
+	{
+		id: 'sdf344f3f34f3f4',
+		name: 'Рундиновивич',
+		location: 'Длинная страна',
+		gender: 'male',
+		age: '33',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, delectus dolore cupiditate tenetur quasi suscipit reprehenderit rem aliquam vero.',
+		avatarUrl: '/assets/images/profile-pictures/Image4.svg',
+		skillCanTeach: [],
+		subcategoriesWantToLearn: [],
+	},
+	{
+		id: 'dsf56sd56fd65fs',
+		name: 'Максим',
+		location: 'Москва',
+		gender: 'male',
+		age: '23',
+		description:
+			'Привет! Люблю ритм, кофе по утрам и людей, которые не боятся пробовать новое',
+		avatarUrl: '/assets/images/profile-pictures/Image3.svg',
+		skillCanTeach: [
+			{
+				skill: 'Бизнес-план',
+				categoryId: 0,
+				subcategory: 0,
+			},
+			{
+				skill: 'Английский язык',
+				categoryId: 1,
+				subcategory: 0,
+			},
+		],
+		subcategoriesWantToLearn: [
+			{
+				skill: 'Бизнес-план',
+				categoryId: 0,
+				subcategory: 0,
+			},
+			{
+				skill: 'Английский язык',
+				categoryId: 1,
+				subcategory: 0,
+			},
+			{
+				skill: 'Тайм менеджмент',
+				categoryId: 2,
+				subcategory: 0,
+			},
+			{
+				skill: 'Медитация',
+				categoryId: 3,
+				subcategory: 0,
+			},
+			{
+				skill: 'Английский язык',
+				categoryId: 1,
+				subcategory: 0,
+			},
+			{
+				skill: 'Тайм менеджмент',
+				categoryId: 2,
+				subcategory: 0,
+			},
+			{
+				skill: 'Медитация',
+				categoryId: 3,
+				subcategory: 0,
+			},
+		],
+	},
+	{
+		id: 'dsf56sd56fd65fs',
+		name: 'Максим',
+		location: 'Москва',
+		gender: 'male',
+		age: '23',
+		description:
+			'Привет! Люблю ритм, кофе по утрам и людей, которые не боятся пробовать новое',
+		avatarUrl: '/assets/images/profile-pictures/Image3.svg',
+		skillCanTeach: [
+			{
+				skill: 'Бизнес-план',
+				categoryId: 0,
+				subcategory: 0,
+			},
+			{
+				skill: 'Английский язык',
+				categoryId: 1,
+				subcategory: 0,
+			},
+		],
+		subcategoriesWantToLearn: [
+			{
+				skill: 'Бизнес-план',
+				categoryId: 0,
+				subcategory: 0,
+			},
+			{
+				skill: 'Английский язык',
+				categoryId: 1,
+				subcategory: 0,
+			},
+			{
+				skill: 'Тайм менеджмент',
+				categoryId: 2,
+				subcategory: 0,
+			},
+			{
+				skill: 'Медитация',
+				categoryId: 3,
+				subcategory: 0,
+			},
+			{
+				skill: 'Английский язык',
+				categoryId: 1,
+				subcategory: 0,
+			},
+			{
+				skill: 'Тайм менеджмент',
+				categoryId: 2,
+				subcategory: 0,
+			},
+			{
+				skill: 'Медитация',
+				categoryId: 3,
+				subcategory: 0,
+			},
+		],
+	},
+];
+
+export const TestPage = () => {
+	return (
+		<>
+			<h1>Страница для тестов</h1>
+			<ul
+				style={{
+					display: 'grid',
+					gridTemplateColumns: '1fr 1fr 1fr 1fr',
+					gap: '24px',
+				}}
+			>
+				{tempUsers?.map((item) => (
+					<li key={item.id}>
+						<CardUser displayMode='default' user={item} />
+					</li>
+				))}
+			</ul>
+		</>
+	);
+};
--- src/shared/ui/tag/Tag.tsx
+++ src/shared/ui/tag/Tag.tsx
@@ -1,1 +1,49 @@
+import clsx from 'clsx';
+import type { ITagProps, SkillCategoriesType } from './Tag.props';
+import styles from './Tag.module.css';
+import { useLayoutEffect, useState } from 'react';
+
+// Описание Компонента
+// проп customBackgroundColor - для установки своего фонового цвета (тогда backgroundColorTemplate дожен быть не задан)
+// проп backgroundColorTemplate - фон из уже опредленных значений категорий (HidedSkills - цвет спрятанных/группированных тегов)
+
+const TagColors: Record<SkillCategoriesType, string> = {
+	HidedSkills: '#E8ECF7',
+	BusinessAndCareer: '#EEE7F7',
+	CreativityAndArt: '#F7E7F2',
+	ForeignLanguages: '#EBE5C5',
+	EducationAndDevelopment: '#E7F2F6',
+	HomeAndComfort: '#F7EBE5',
+	HealthAndLifestyle: '#E9F7E7',
+} as const;
+
 // Универсальный компонент тега (Tag) для отображения категорий/меток
+export const Tag = ({
+	customBackgroundColor = '#FFF',
+	children,
+	backgroundColorTemplate,
+	className,
+	...props
+}: ITagProps) => {
+	const [backgroundColor, setBackgroundColor] = useState(customBackgroundColor);
+
+	useLayoutEffect(() => {
+		if (backgroundColorTemplate) {
+			setBackgroundColor(() => {
+				return TagColors[backgroundColorTemplate];
+			});
+		}
+	}, []);
+
+	return (
+		<div
+			className={clsx(className, styles.tag)}
+			style={{
+				backgroundColor,
+			}}
+			{...props}
+		>
+			{children}
+		</div>
+	);
+};
